f1e2198a68bec8d410e4bf6a1ae1a9bb9b7ad2f0
ninaniel <shubitidzenina@gmail.com>
2025-08-14 18:58:01 +0400

    update demos task

--- src/main/java/ge/tbc/testautomation/data/Constants.java
+++ src/main/java/ge/tbc/testautomation/data/Constants.java
@@ -3,4 +3,13 @@ package ge.tbc.testautomation.data;
 public class Constants {
     public static final String BASE_TELERIK_URL = "https://www.telerik.com/";
     public static final String SEARCH_INPUT = "Kendo UI";
+
+    // task - validateDemosDesign
+    public static final String DEMOS_URL = "https://www.telerik.com/support/demos";
+
+    public static final String KENDO_UI_VUE_DEMOS = "UI for Vue Demos";
+
+    public static final String MICROSOFT_STORE = "Microsoft Store";
+    public static final String APPLE_STORE = "Apple Store";
+    public static final String GOOGLE_PLAY = "Google Play";
 }
